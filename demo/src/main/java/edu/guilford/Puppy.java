5666a647a1ee10f4caedb69fb00d09d06672eaec
Tori Williams <vwilliams@guilford.edu>
2023-04-20 17:00:07 -0400

    puppy project

--- demo/src/main/java/edu/guilford/Puppy.java
+++ demo/src/main/java/edu/guilford/Puppy.java
@@ -1,5 +1,7 @@
 package edu.guilford;
 
+import java.util.Random;
+
 public class Puppy {
     
     //attributes
@@ -8,26 +10,138 @@ public class Puppy {
     private String breed;
     private String color;
     private String furtype;
+    private String size;
     private double weight;
     private boolean spots;
-    private boolean female;
+    private String sex;
+    
+    Random rand = new Random();
 
     //constructor
-    public Puppy (String name, int age, String breed, String color, String furtype, double weight, boolean spots, boolean female) {
+    public Puppy (String name, int age, String breed, String color, String furtype, String size, double weight, boolean spots, String sex) {
         this.name = name;
         this.age = age;
         this.breed = breed;
         this.color = color;
         this.furtype = furtype;
+        this.size = size;
         this.weight = weight;
         this.spots = spots;
-        this.female = female;
+        this.sex = sex;
     }
 
+    //random constructor that sets the attributes to random values
     public Puppy () {
-        
+        this.age = rand.nextInt(10);
+        this.weight = rand.nextDouble() * 100;
+        this.spots = rand.nextBoolean();
+
+    // generate a random integer between 0 and 8 to use as an index for the array for the random name
+    int nameChoice =rand.nextInt(8);
+    if (nameChoice == 0) {
+        name = "Lucky";
+    }
+    else if (nameChoice == 1) {
+        name = "Buddy";
+    }
+    else if (nameChoice == 2) {
+        name = "Charlie";
+    }
+    else if (nameChoice == 3) {
+        name = "Rocky";
+    }
+    else if (nameChoice == 4) {
+        name = "Bella";
+    }
+    else if (nameChoice == 5) {
+        name = "Daisy";
+    }
+    else if (nameChoice == 6) {
+        name = "Molly";
+    }
+    else if (nameChoice == 7) {
+        name = "Sissy";
     }
 
+    // generate a random integer between 0 and 10 to use as an index for the array for the random breed
+    int breedChoice =rand.nextInt(10);
+    if (breedChoice == 0) {
+        breed = "Beagle";
+    }
+    else if (breedChoice == 1) {
+        breed = "Labrador";
+    }
+    else if (breedChoice == 2) {
+        breed = "Yorkie";
+    }
+    else if (breedChoice == 3) {
+        breed = "Husky";
+    }
+    else if (breedChoice == 4) {
+        breed = "Chihuahua";
+    }
+    else if (breedChoice == 5) {
+        breed = "Bulldog";
+    }
+    else if (breedChoice == 6) {
+        breed = "German Sheperd";
+    }
+    else if (breedChoice == 7) {
+        breed = "Pomeranian";
+    }
+    else if (breedChoice == 8) {
+        breed = "Jack Russell";
+    }
+    else if (breedChoice == 9) {
+        breed = "Poodle";
+    }
+
+    // generate a random integer between 0 and 4 to use as an index for the array for the random color
+    int colorChoice = rand.nextInt(4);
+    if (colorChoice == 0) {
+        color = "Black";
+    }
+    else if (colorChoice == 1) {
+        color = "Brown";
+    }
+    else if (colorChoice == 2) {
+        color = "White";
+    }
+    else if (colorChoice == 3) {
+        color = "Red";
+    }
+
+    // generate a random integer between 0 and 2 to use as an index for the array for the random furtype
+    int furtypeChoice = rand.nextInt(2);
+    if (furtypeChoice == 0) {
+        furtype = "Short";
+    }
+    else if (furtypeChoice == 1) {
+        furtype = "Long";
+    }
+
+    // generate a random integer between 0 and 2 to use as an index for the array for the random size
+    int sizeChoice = rand.nextInt(2);
+    if (sizeChoice == 0) {
+        size = "Small";
+    }
+    else if (sizeChoice == 1) {
+        size = "Medium";
+    }
+    else if (sizeChoice == 2) {
+        size = "Large";
+    }
+   
+    // generate a random integer between 0 and 1 to use as an index for the array for the random size
+    int sexChoice = rand.nextInt(1);
+    if (sexChoice == 0) {
+        sex = "Female";
+    }
+    else if (sexChoice == 1) {
+        sex = "Male";
+    }
+
+}
 
     //getters
     public String getName() {
@@ -45,14 +159,17 @@ public class Puppy {
     public String getFurtype() {
         return furtype;
     }
+    public String getSize() {
+        return size;
+    }
     public double getWeight() {
         return weight;
     }
     public boolean isSpots() {
         return spots;
     }
-    public boolean isFemale() {
-        return female;
+    public String getSex() {
+        return sex;
     }
 
     //setters
@@ -71,14 +188,17 @@ public class Puppy {
     public void setFurtype(String furtype) {
         this.furtype = furtype;
     }
+    public void setSize(String size) {
+        this.size = size;
+    }
     public void setWeight(double weight) {
         this.weight = weight;
     }
     public void setSpots(boolean spots) {
         this.spots = spots;
     }
-    public void setFemale(boolean female) {
-        this.spots = female;
+    public void setSex(String sex) {
+        this.sex = sex;
     }
 
 
@@ -92,5 +212,10 @@ public class Puppy {
     public void cuddle() {
         System.out.println("Pet me!");
     }
+    public void play() {
+        System.out.println("Let's play!");
+    }
+
+
 
 }
--- demo/src/main/java/edu/guilford/PuppyProject.java
+++ demo/src/main/java/edu/guilford/PuppyProject.java
@@ -1,5 +1,7 @@
 package edu.guilford;
 
+import java.text.DecimalFormat;
+
 /**
  * Hello world!
  *
@@ -19,9 +21,10 @@ public class PuppyProject
     myPuppy.setBreed("Pomeranian");
     myPuppy.setColor("Black");
     myPuppy.setFurtype("Short");
+    myPuppy.setSize("Small");
     myPuppy.setWeight(5.0);
     myPuppy.setSpots(false);
-    myPuppy.setFemale(true);
+    myPuppy.setSex("Female");
 
     //print the attributes of the Puppy object
     System.out.println("Name: " + myPuppy.getName());
@@ -29,9 +32,10 @@ public class PuppyProject
     System.out.println("Breed: " + myPuppy.getBreed());
     System.out.println("Color: " + myPuppy.getColor());
     System.out.println("Fur Type: " + myPuppy.getFurtype());
+    System.out.println("Size: " + myPuppy.getSize());
     System.out.println("Weight: " + myPuppy.getWeight());
     System.out.println("Spots: " + myPuppy.isSpots());
-    System.out.println("Female: " + myPuppy.isFemale());
+    System.out.println("Female: " + myPuppy.getSex());
 
     //call the bark method
     myPuppy.bark();
@@ -41,6 +45,26 @@ public class PuppyProject
 
     //call the eat method
     myPuppy.eat();
+
+    // call the play method
+    myPuppy.play();
+
+    //instantiate a random Puppy object
+    Puppy randomPuppy = new Puppy();
+
+    //use the constructor to randomly set the attributes of the Puppy object
+    DecimalFormat formatter = new DecimalFormat("0.00");
+    System.out.println("\nRandom Puppy's Attributes: ");
+    System.out.println("Name: " + randomPuppy.getName());
+    System.out.println("Age: " + randomPuppy.getAge());
+    System.out.println("Breed: " + randomPuppy.getBreed());
+    System.out.println("Color: " + randomPuppy.getColor());
+    System.out.println("Fur Type: " + randomPuppy.getFurtype());
+    System.out.println("Size: " + randomPuppy.getSize());
+    System.out.println("Weight: " + formatter.format(randomPuppy.getWeight()));
+    System.out.println("Spots: " + randomPuppy.isSpots());
+    System.out.println("Female: " + randomPuppy.getSex());
+
 
     }
 }
